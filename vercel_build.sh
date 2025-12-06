06a679a0218821cbd532ffa4d1738025b0908ed8
NwazeChukwuka <nwazemoses@gmail.com>
2025-12-06 09:58:41 +0100

    Fix: upgraded Flutter for Dart 3.9 and added safe.directory

--- vercel_build.sh
+++ vercel_build.sh
@@ -4,11 +4,16 @@ apt-get update
 
 apt-get install -y curl git xz-utils zip libglu1-mesa
 
-curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.3-stable.tar.xz
+# Download newer Flutter version that includes Dart >= 3.9.2
+curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.27.0-stable.tar.xz
 
-tar xf flutter_linux_3.24.3-stable.tar.xz
+tar xf flutter_linux_3.27.0-stable.tar.xz
 
+# Add flutter to PATH
 export PATH="$PATH:$(pwd)/flutter/bin"
+
+# Fix git ownership issue for flutter directory
+git config --global --add safe.directory $(pwd)/flutter
 
 flutter --version
 
